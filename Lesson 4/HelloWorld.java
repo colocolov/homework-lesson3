88a6055b66f42bb6e670fa654b017f4cff392a7c
Colocolov Stanislav <colocolov.stas@gmail.com>
2019-01-25 09:19:32 +0200

    Second commit. Added folder Lesson 4

---
+++ Lesson 4/HelloWorld.java
@@ -0,0 +1,11 @@
+//import java.util.Scanner;
+
+public class HelloWorld {
+	
+	public static void main(String[] args){
+		//String version = System.getProperty("java.version");
+		System.out.println("\nHello");
+		System.out.println("\nJava Fundamentals, Tekwill");
+		System.out.println("\nColocolov Stanislav");	
+ 	}
+}
